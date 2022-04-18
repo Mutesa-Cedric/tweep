91679e9ea8d459a77439c431ad5e8adeaf78a387
Mutesa-Cedric <96414111+Mutesa-Cedric@users.noreply.github.com>
2022-04-18 14:32:20 +0200

    finished signup

--- server/controllers/authControllers/signup.controller.js
+++ server/controllers/authControllers/signup.controller.js
@@ -8,60 +8,66 @@ const saltRounds=10
 //register function
 
 const register=async(req,res)=>{
-
     try{
         const {userName,email,password}=req.body;
 
         //checking if username is teaken
-        const existingUser= await usersSchema.findOne({email:email});
-        if(existingUser){
+        const existingUserName= await usersSchema.findOne({userName:userName})
+        if(existingUserName){
             res.json({
                 status:400,
-                userExist:true,
-                message:"user with a given email already exists"
+                existingUserName:true,
+                message:"user with a given Username already exists"
             })
-        }else{
-
-            
-            bcrypt.hash(password,saltRounds,async(err,hash)=>{
-                if(err){
-                    throw err;
-                }else{
-
-                    const user=new usersSchema({
-                        userName:userName,
-                        email:email,
-                        password:hash
-                     })
-
-                 await user.save()
-
-                 let userToSign={
-                     userName:userName,
-                     email:email
-                 }
-                 const accessToken=jwt.sign(userToSign,process.env.ACCESS_KEY_SECRET)
-
-                 console.log(accessToken)
-                 if(user){
-                    let message=`${process.env.VERIFY_URL}/${user._id}`
-                    sendEmail(email,"verify your account",message)
-                    res.json({
-                        status:201,
-                        success:true,
-                        message:"user created successfully!"
-                    })
-                 }
-
-                }
-            })
+        }
+        else{  
+            const existingEmail= await usersSchema.findOne({email:email});
+            if(existingEmail){
+                res.json({
+                    status:400,
+                    existingEmail:true,
+                    message:"user with a given email already exists"
+                })
+            }
+            else{
+                bcrypt.hash(password,saltRounds,async(err,hash)=>{
+                    if(err){
+                        throw err;
+                    }else{
+    
+                        const user=new usersSchema({
+                            userName:userName,
+                            email:email,
+                            password:hash
+                         })
+    
+                        await user.save()
+        
+                        let userToSign={
+                            userName:userName,
+                            email:email
+                        }
+                        const accessToken=jwt.sign(userToSign,process.env.ACCESS_KEY_SECRET)
+        
+                        if(user){
+                            // let message=`${process.env.VERIFY_URL}/${user._id}`
+                            // sendEmail(email,"verify your account",message)
+                            res.json({
+                                status:201,
+                                success:true,
+                                message:"user created successfully!",
+                                accessToken:accessToken
+                            })
+                         }
+                    }
+                })
+            }
+           
         }
     }catch(err){
         console.log(err)
     }
-  
 }
-
 //register function
 
 module.exports.register=register;

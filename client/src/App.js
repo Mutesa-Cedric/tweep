bcc59b8a2dd2aa1f5a51713ab4f23acd33b46f07
Mutesa-Cedric <96414111+Mutesa-Cedric@users.noreply.github.com>
2022-04-19 18:58:24 +0200

    added new routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Login from './pages/Login';
-import Signup from './pages/Signup';
+import SignupMain from './pages/signup/Main';
 import Home from './pages/Home';
 import Explore from "./pages/Explore";
 import Bookmarks from './pages/Bookmarks';
+import Chat from './pages/Chat';
+import Profile from "./pages/Profile"
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import React,{useState,useEffect} from 'react';
+import React,{useState} from 'react';
 
 function App() {
 
@@ -20,10 +22,12 @@ function App() {
         <div>
           <Routes>
             <Route path='/auth/login' exact element={<Login darkMode={darkMode} setDarkMode={toggleDarkMode}/>} />
-            <Route path='/auth/signup' exact element={<Signup darkMode={darkMode} setDarkMode={toggleDarkMode} />} />
+            <Route path='/auth/signup' exact element={<SignupMain darkMode={darkMode} setDarkMode={toggleDarkMode} />} />
             <Route path='/bookmarks' exact element={<Bookmarks/>}/>
             <Route path='/explore' exact element={<Explore/>}/>
             <Route path='/' exact element={<Home/>}/>
+            <Route path='/chat' element={<Chat/>}/>
+            <Route path='/profile' element={<Profile/>}/>
           </Routes>  
         </div>
       </Router>  

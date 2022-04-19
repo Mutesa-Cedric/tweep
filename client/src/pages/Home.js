76fbac398b37731bc0f6332cc27685704f17fad0
Mutesa-Cedric <96414111+Mutesa-Cedric@users.noreply.github.com>
2022-04-19 19:01:40 +0200

    modified home pages

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,32 @@
+import Navbar from "../components/Navbar";
+import Post from "../components/Post";
+import TrendsForYou from "../components/TrendsForYou";
+import WhoToFollow from "../components/WhoToFollow";
+import TweepSomething from "../components/TweepSomething";
+import mui from '@mui/icons-material'
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
 let Home=()=>{
     return (
-        <div>
-            <h1>this is home page</h1>
+        <div className="bg-[#F2F2F2] h-auto overflow-x-hidden">
+            <Navbar/>
+            <div className="w-full h-3/4 mt-10 px-52">
+                <div className="w-full h-auto flex justify-between">
+                    {/* main */}
+                    <div>
+                        <TweepSomething />
+                        <Post/>
+                        <Post/>
+                    </div>
+                    {/* main */}
+                    {/* side banners */}
+                    <div className="">
+                         <TrendsForYou/>
+                         <WhoToFollow/> 
+                         <ChatBubbleOutlineIcon/>  
+                    </div>    
+                    {/* side banners */}
+                </div>
+            </div>
         </div>
     )
 }

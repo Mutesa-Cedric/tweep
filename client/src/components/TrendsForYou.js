c2c6c38f54eab3ef909b3402bf2305d43c9550ba
Mutesa-Cedric <96414111+Mutesa-Cedric@users.noreply.github.com>
2022-04-20 14:30:08 +0200

    added TrendsForYou component

---
+++ client/src/components/TrendsForYou.js
@@ -0,0 +1,29 @@
+
+let TrendsForYou=(props)=>{
+    return (
+        <div className="w-[306px] h-[495px] bg-white shadow-sm rounded-xl px-6">
+            <div className="w-full border-b-[1.5px]">
+                <p className="mb-2 pt-3 font-[600] text-[14px] ">Trends for you</p>
+            </div>
+            <div className="my-5">
+                <h1 className="font-[600] text-[#333333] mb-2">#programming</h1>
+                <p className="text-[#828282] font-medium text-[14px]">213k Tweeps</p>
+            </div>
+            <div className="my-5">
+                <h1 className="font-[600] text-[#333333] mb-2">#musicproduction</h1>
+                <p className="text-[#828282] font-medium text-[14px]">178k Tweeps</p>
+            </div> <div className="my-5">
+                <h1 className="font-[600] text-[#333333] mb-2">#amiricangottalent</h1>
+                <p className="text-[#828282] font-medium text-[14px]">220k Tweeps</p>
+            </div> <div className="my-5">
+                <h1 className="font-[600] text-[#333333] mb-2">#ukrainevsrussia</h1>
+                <p className="text-[#828282] font-medium text-[14px]">107k Tweeps</p>
+            </div> <div className="my-5">
+                <h1 className="font-[600] text-[#333333] mb-2">#learnpython</h1>
+                <p className="text-[#828282] font-medium text-[14px]">100k Tweeps</p>
+            </div>                  
+        </div>
+    )
+}
+
+export default TrendsForYou;

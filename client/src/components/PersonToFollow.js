5654fd5af6d7c77b2772fae66f6f1b42a6676240
Mutesa-Cedric <96414111+Mutesa-Cedric@users.noreply.github.com>
2022-04-21 10:05:48 +0200

    added dark mode

--- client/src/components/PersonToFollow.js
+++ client/src/components/PersonToFollow.js
@@ -1,3 +1,4 @@
+import PersonAddAlt1OutlinedIcon from '@mui/icons-material/PersonAddAlt1Outlined';
 
 let PersonToFollow=(props)=>{
     return (
@@ -6,18 +7,18 @@ let PersonToFollow=(props)=>{
                 <div className="flex items-center">
                     <img src={props.profile} alt="woman" className="rounded-md mr-4  h-10 w-10"/>
                     <div className="flex flex-col">
-                        <h1 className="font-medium">{props.name}</h1>
+                        <h1 className={props.darkMode?"font-medium text-white":"font-medium"}>{props.name}</h1>
                         <p className="text-gray-300  text-[14px]">{props.followers}k followers</p>
                     </div>
                 </div>
                 <div className="flex items-center">
-                    <button className="text-white bg-[#2F80ED] px-4 py-1 rounded-md capitalize">
-                        follow
+                    <button className="text-white bg-[#2F80ED] px-4 py-1 flex items-center justify-between rounded-md capitalize">
+                        <PersonAddAlt1OutlinedIcon fontSize="small" className="mr-1"/> follow
                     </button>
                 </div>
             </div>
             <div className="flex items-center my-2">
-                <p className="text-[#828282] text-[14px]">{props.bio}</p>
+                <p className={props.darkMode?"text-gray-300 text-[14px]":"text-[#828282] text-[14px]"}>{props.bio}</p>
             </div>
             <div className={props.isFirst ? "border-b-2":""}>
                 <img src={props.coverPhoto}  alt="cover" className="rounded-[8px] my-4 w-[265px] h-32 "/>

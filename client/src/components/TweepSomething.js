83ce5a70bfe3f71f9f4d6d5605c2ed20b2043e55
Mutesa-Cedric <96414111+Mutesa-Cedric@users.noreply.github.com>
2022-04-21 10:06:21 +0200

    added dark mode

--- client/src/components/TweepSomething.js
+++ client/src/components/TweepSomething.js
@@ -1,16 +1,20 @@
 import ManImg from "../images/Man.jpg";
+import PublicIcon from '@mui/icons-material/Public';
+import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
 let TweepSomething=(props)=>{
     return (
-        <div className="w-[745px] mb-10 bg-white h-[155px] shadow-sm rounded-xl px-6">
+        <div className={props.darkMode?"w-[745px] mb-10 bg-inherit  h-[155px] shadow-xl rounded-xl px-6":"w-[745px] mb-10 bg-white h-[155px] shadow-sm rounded-xl px-6"}>
             <div className="w-full border-b-[1.5px]">
-                <p className="text-[#4F4F4F] font-[600] text-[14px] py-2">Tweep something</p>
+                <p className={props.darkMode?"font-[600] text-[14px] py-2 text-white":"text-[#4F4F4F] font-[600] text-[14px] py-2"}>Tweep something</p>
             </div>
             <div className="flex items-center mt-3">
                 <img src={ManImg} alt="profile" className="w-[36px] h-[36px] rounded-md mr-4"/>
-                <input placeholder="what 's happening?" className="placeholder:text-[#BDBDBD]"/>
+                <input placeholder="what 's happening?" className="placeholder:text-[#BDBDBD] bg-inherit"/>
             </div>
             <div className="w-full mt-6 h-auto pl-12 flex items-center justify-between">
-                <div className="w-full ">
+                <div className="w-full flex items-center">
+                    <ImageOutlinedIcon style={{fill:"#2F80ED"}} fontSize="small" className="cursor-pointer"/>
+                    <PublicIcon style={{fill:"#2F80ED"}} fontSize="small" className="mx-2"/>
                     <p className="text-[#2F80ED] text-[14px] cursor-pointer font-medium">Everyone can reply</p>
                 </div>
                 <div>

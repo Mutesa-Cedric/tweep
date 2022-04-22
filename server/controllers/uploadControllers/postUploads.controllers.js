7124914dcb8d9f4fec8b56ec4ccab6f619387b13
Mutesa-Cedric <96414111+Mutesa-Cedric@users.noreply.github.com>
2022-04-22 19:15:49 +0200

    finished post upload and storage with multer

---
+++ server/controllers/uploadControllers/postUploads.controllers.js
@@ -0,0 +1,46 @@
+const multer=require('multer');
+const path=require('path');
+
+ let time=new Date().getTime();
+ //multer storage for media
+ let storage=multer.diskStorage({
+     destination:path.join(__dirname,'../../uploads/posts/'),
+     filename:(req,file,cb)=>{
+         cb(null,req.body.postedBy+time+'.png')
+     }
+ })
+ //configuring uploads
+ let upload=multer({
+     storage:storage,
+     limits:{
+         fileSize:8000000
+     },
+     fileFilter:(req,file,cb)=>{
+         let fileTypes=/jpg|jpeg|png|gif/;
+         let extName=fileTypes.test(path.extname(file.originalname).toLowerCase());
+         let mimeType=fileTypes.test(file.mimetype);
+         if(fileTypes && mimeType){
+             cb(null,true)
+         }else{
+             cb('error:images only are allowed')
+         }
+     }
+ });
+ //exporting multer function
+let uploadMedia=(req,res,next) => {
+    upload.single('media')(req,res,(err)=>{
+        req.time=time;
+        if(err){
+            return res.json({
+                status:500,
+                success:false,
+                message:err
+            })
+        }else{
+            next();
+        }
+    })
+}
+
+module.exports.uploadMedia = uploadMedia
+

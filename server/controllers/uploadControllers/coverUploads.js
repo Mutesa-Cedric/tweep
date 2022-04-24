87ae5207701d0a839d4faeed652e9fb052bb5b6a
Mutesa-Cedric <96414111+Mutesa-Cedric@users.noreply.github.com>
2022-04-24 15:13:41 +0200

    finished cover image upload controllers

---
+++ server/controllers/uploadControllers/coverUploads.js
@@ -0,0 +1,42 @@
+const multer=require('multer');
+const path=require('path');
+const storage=multer.diskStorage({
+    destination:path.join(__dirname,'../../uploads/covers/'),
+     filename:(req,file,cb)=>{
+         cb(null,req.params.userName+'Cover'+'.png')
+     }
+})
+
+
+const upload=multer({
+    storage:storage,
+    limits:{
+        fileSize:8000000
+    },
+    fileFilter:(req,file,cb)=>{
+        let fileTypes=/jpg|jpeg|png|gif/;
+        let extName=fileTypes.test(path.extname(file.originalname).toLowerCase());
+        let mimeType=fileTypes.test(file.mimetype);
+        if(fileTypes && mimeType){
+            cb(null,true)
+        }else{
+            cb('error:images only are allowed')
+        }
+    }
+})
+
+let uploadCover=(req,res,next)=>{
+    upload.single("coverImg")(req,res,(err)=>{
+       if(err){
+        return res.json({
+            status:500,
+            success:false,
+            message:err
+        })
+       }else{
+           next()
+       }
+    })
+}
+
+module.exports.uploadCover=uploadCover;

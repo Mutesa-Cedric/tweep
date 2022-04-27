7d6eafd41083b61178628240167d987079ebcd4f
Mutesa-Cedric <96414111+Mutesa-Cedric@users.noreply.github.com>
2022-04-27 21:04:17 +0200

    login verification

--- server/controllers/authControllers/Login.controller.js
+++ server/controllers/authControllers/Login.controller.js
@@ -4,41 +4,40 @@ const bcrypt=require("bcrypt");
 const login=async(req, res)=>{
     try{
         const {email,password} = req.body;
-        const user=await usersSchema.findOne({email: email})
-        if(!user){
-
-            res.json({
-                status:404,
-                isFound:false,
-                message:"user not found"
-            })
-        }else{
-          bcrypt.compare(password,user.password,async(error,result)=>{
-                if(error){
-                    console.log(error);
-                }else{
-                    if(result){
-                        let userToSign={
-                            userName:user.userName,
-                            email:email
-                        }
-                        const accessToken=jwt.sign(userToSign,process.env.ACCESS_KEY_SECRET)
-                        res.json({
-                            status:200,
-                            loggedIn:true,
-                            accessToken:accessToken
-                        })
+        const user=await usersSchema.findOne({email: email},(err,user)=>{
+            if(err){
+                res.json({
+                    status:404,
+                    isFound:false,
+                    message:"user not found"
+                })
+            }else{
+                bcrypt.compare(password,user.password,(error,result)=>{
+                    if(error){
+                        console.log(error);
                     }else{
-                        res.json({
-                            status:404,
-                            passwordMatch:false,
-                            message:"incorrect password"
-                        })
+                        if(result){
+                            let userToSign={
+                                userName:user.userName,
+                                email:email
+                            }
+                            const accessToken=jwt.sign(userToSign,process.env.ACCESS_KEY_SECRET)
+                            res.json({
+                                status:200,
+                                loggedIn:true,
+                                accessToken:accessToken
+                            })
+                        }else{
+                            res.json({
+                                status:404,
+                                passwordMatch:false,
+                                message:"incorrect email or password"
+                            })
+                        }
                     }
-                }
-           })
-            
-        }  
+               })
+            }
+        }) 
     }catch(err){
         console.log(err);
     }

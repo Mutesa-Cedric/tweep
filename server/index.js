554215091647e2babc20217715fc9c7a390baddf
Mutesa-Cedric <96414111+Mutesa-Cedric@users.noreply.github.com>
2022-04-30 21:53:25 +0200

    hsb

--- server/index.js
+++ server/index.js
@@ -4,15 +4,38 @@ const express=require('express');
 const app=express();
 const cors=require('cors');
 const bodyParser = require('body-parser');
-const urlencoded = require('body-parser/lib/types/urlencoded');
+require("dotenv").config()
+const mongoose=require("mongoose")
+const {Router}=require("./routes/router")
+const exphbs=require("express-handlebars")
+const path=require("path")
+//libraries and modules
 
 //use
 app.use(cors());
-// app.use(bodyParser(urlencoded=true))
+app.use(bodyParser.json())
+app.use(express.json());
+app.use("/",Router)
+app.use(express.static(path.join(__dirname, "./uploads/posts")));
+app.use(express.static(path.join(__dirname, "./uploads/profiles")))
+app.use(express.static(path.join(__dirname, "./uploads/covers")))
 
 //use
 
-const port=7070
+//database connection
+const databaseUrl=process.env.DATABASE_URL
+mongoose.connect(databaseUrl,(err)=>{
+    if(err){    
+        console.log(err)
+    }else{
+        console.log("database connection established")
+    }
+})
+//database connection
+
+
+const port=process.env.PORT
+
 app.listen(port,()=>{
     console.log('listening on port '+ port);
 })

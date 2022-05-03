80479c3f659e801cec3c13c5cd722d7c47bf626f
Mutesa-Cedric <96414111+Mutesa-Cedric@users.noreply.github.com>
2022-05-03 17:24:45 +0200

    commenting

--- client/src/components/TweepSomething.js
+++ client/src/components/TweepSomething.js
@@ -2,38 +2,50 @@ import PublicIcon from '@mui/icons-material/Public';
 import PersonIcon from '@mui/icons-material/Person';
 import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
 import CancelIcon from '@mui/icons-material/Cancel';
+import {Modal} from "@mui/material";
+import React,{useState} from "react";
 
 let TweepSomething=(props)=>{
     let postForm =document.getElementById("postForm")
     let file=document.getElementById("file")
+    const [finishedPosting, setFinishedPosting] = useState(false);
     // console.log(file.files[0])
     const handlePostSubmit=(e)=>{
         e.preventDefault()
        let formData=new FormData(postForm)
-       formData.append('postedBy',props.userName);
+        formData.append('postedAt',`${new Date().getTime()}`)
+        formData.append('postedBy',props.userName);
        formData.append("media",file.files[0])
 
        fetch("http://localhost:7070/posts/newPost/",{
            method:'POST',
-        //  headers:{'Content-Type': 'multipart/form-data'},
            body:formData
        }).then(response=>response.json())
        .then(data=>{
-           console.log(data)
+            if(data.saved===true){
+                props.updateDomPost()
+                let textArea=document.getElementById('textarea')
+                textArea.textContent=null
+                textArea.value=null
+                props.cancelImage()
+                props.finishPosting()
+            }
        }).catch(err=>console.log(err))
     }
     return (
         <div className={props.darkMode?"w-auto mb-10 bg-inherit  h-auto shadow-xl rounded-xl sm:mx-4 px-6":"w-auto sm:mx-4 mb-10 bg-white h-auto shadow-sm rounded-xl px-6"}>
+
+            {/*/!*{finishedPosting && <Modal keepMounted />*!/*/}
+            {/*}*/}
             <div className="w-full border-b-[1.5px]">
                 <p className={props.darkMode?"font-[600] text-[14px] py-2 text-white":"text-[#4F4F4F] font-[600] text-[14px] py-2"}>Tweep something</p>
             </div>
-            <form id="postForm" onSubmit={handlePostSubmit} onLoad={(e)=>e.preventDefault()} enctype="multipart/form-data">
+            <form id="postForm" onSubmit={handlePostSubmit} onLoad={(e)=>e.preventDefault()} encType="multipart/form-data">
               <div className="flex items-center mt-3">
                 {props.profileImg?<img src={props.profileImg} alt="profile" className="w-[36px] h-[36px] rounded-md mr-4"/>:<PersonIcon fontSize="large" className=" rounded-[50%] w-[36px] h-[36px] mr-4 bg-gray-200" style={{fill:"#808080"}}/>}
                 <textarea placeholder="what 's happening?" className={props.darkMode?"placeholder:text-[#BDBDBD] focus:outline-none text-white border-none w-full h-full bg-inherit":"placeholder:text-[#BDBDBD] focus:outline-none border-none w-full h-full bg-inherit"} 
-                onInput={props.handlePostText}
                 name="text"
-                // value={props.postData.text}
+                id={"textarea"}
                 />
             </div>
             <div className="w-full mt-6 h-auto pl-12 flex items-center justify-between">

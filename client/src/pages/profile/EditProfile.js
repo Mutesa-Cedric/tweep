8a35686671c2bf7c954e64e14935ceb32713d78a
Mutesa-Cedric <96414111+Mutesa-Cedric@users.noreply.github.com>
2022-05-05 08:29:55 +0200

    enhanced profile edit

--- client/src/pages/profile/EditProfile.js
+++ client/src/pages/profile/EditProfile.js
@@ -22,6 +22,9 @@ const EditProfile = (props) => {
     const [isCropping, setIsCropping] = useState(false);
     const [profileToCrop, setProfileToCrop] = useState(null);
     const [coverToCrop, setCoverToCrop] = useState(null);
+    const [finishedProfile, setFinishedProfile] = useState(false);
+    const [finishedCover, setFinishedCover] = useState(false);
+
 
     //states
 
@@ -53,8 +56,8 @@ const EditProfile = (props) => {
     //profile image
 
     //post image
+    let coverImageForm = document.getElementById('coverImageForm');
     function postCover(coverImage) {
-        let coverImageForm = document.getElementById('coverImageForm');
         let formData = new FormData(coverImageForm)
         formData.append('coverImg', coverImage)
         fetch(`http://localhost:7070/profiles/cover/${props.userProfile.userName}`, {
@@ -147,10 +150,10 @@ const EditProfile = (props) => {
     // let myProfile=document.getElementById('profileImage')
     //editing profile
     useEffect(() => {
-        if(profileImage !== props.userProfile.profileImage){
+        if (profileImage !== props.userProfile.profileImage) {
             setIsCropping(true)
-            let fileReader=new FileReader()
-            fileReader.onload=(e)=>{
+            let fileReader = new FileReader()
+            fileReader.onload = (e) => {
                 setProfileToCrop(e.target.result)
             }
             fileReader.readAsDataURL(profileImage)
@@ -161,11 +164,11 @@ const EditProfile = (props) => {
     }, [profileImage])
 
     //editing cover
-    useEffect(()=>{
-        if(coverImage !== props.userProfile.coverImage && coverImage !== null){
-            setIsCropping(true)
-            let fileReader=new FileReader()
-            fileReader.onload=(e)=>{
+    useEffect(() => {
+        if (coverImage !== props.userProfile.coverImage && coverImage !== null) {
+            setIsCropping((prevState) => !prevState)
+            let fileReader = new FileReader()
+            fileReader.onload = (e) => {
                 setCoverToCrop(e.target.result)
             }
             fileReader.readAsDataURL(coverImage)
@@ -173,28 +176,24 @@ const EditProfile = (props) => {
             setCropCover(true)
             setCropProfile(false)
         }
-    },[coverImage])
+    }, [coverImage])
 
 
     //finish editing
 
-    const finishEditingProfile=()=>{
+    const finishEditingProfile = () => {
         setIsCropping(false)
-        setCoverToCrop(null)
-        document.getElementById("profileImageHolder").style.backgroundImage=`url(${profileToCrop})`
-        // setProfileImage(profileToCrop)
+        setFinishedProfile(true)
     }
 
-    const finishEditingCover=()=>{
+    const finishEditingCover = () => {
         setIsCropping(false)
-        setProfileToCrop(null)
-        document.getElementById('coverImageHolder').style.backgroundImage=`url(${coverToCrop})`
-        // setCoverImage(coverToCrop)
+        setFinishedCover(true)
     }
 
     //cancel editing
 
-    const cancelEditing=()=>{
+    const cancelEditing = () => {
         setIsCropping(false)
         setCropProfile(false)
         setCropCover(false)
@@ -210,8 +209,11 @@ const EditProfile = (props) => {
 
     if (isCropping) {
         return <>
-            {cropProfile && <PreviewImage hasFile={true} aspect={2/1.3} finishEditing={finishEditingProfile} message={'Profile image'} image={profileToCrop}/>}
-            {cropCover && <PreviewImage hasFile={true} aspect={2/2} message={'Cover image'} finishEditing={finishEditingCover} image={coverToCrop}/>}
+            {cropProfile && <PreviewImage hasFile={true} aspect={2 / 1.3} finishEditing={finishEditingProfile}
+                                          message={'Profile image'} image={profileToCrop}/>}
+            {cropCover &&
+                <PreviewImage hasFile={true} aspect={2 / 2} message={'Cover image'} finishEditing={finishEditingCover}
+                              image={coverToCrop}/>}
         </>
     }
 
@@ -219,57 +221,85 @@ const EditProfile = (props) => {
         <div>
             <div className="fixed z-[100] inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog"
                  aria-modal="true">
-
                 <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
                     <div
                         className="fixed justify-center items-center inset-0 bg-gray-600 bg-opacity-75 transition-opacity"
                         aria-hidden="true">
                         <div
                             className={"border-2 border-gray-400  bg-gray-800 shadow-xl flex flex-col w-2/6 h-[75%] mx-auto my-24 rounded-xl"}>
-                            <div className={"bg-cover bg-no-repeat bg-center rounded-t-xl h-56 relative"}
-                                 id={"coverImageHolder"}
-                                 style={{backgroundImage: `url(http://localhost:7070/${props.userProfile.coverImage})`}}>
-                                <div
-                                    className={'absolute px-4 w-full flex  items-center justify-between top-2 font-bold text-white z-10'}>
-                                    <KeyboardBackspaceIcon fontSize={'large'}
-                                                           className={'cursor-pointer hover:bg-white hover:text-gray-800 rounded-2xl text-gray-200'}
-                                                           onClick={props.toggleIsEditing}/>
-                                    <button
-                                        className={'bg-white text-gray-900 text-xl px-4 py-[2px] rounded-2xl hover:text-white hover:bg-gray-800 flex items-center'}
-                                        onClick={controlPosting}>
-                                        <span>Save</span>
-                                    </button>
+                            {/*{finishedCover ?*/}
+                            {/*    <div className={"bg-cover bg-no-repeat bg-center rounded-t-xl h-56 relative"}>*/}
+                            {/*        <img src={coverToCrop} alt="cover" className="w-full h-full"/>*/}
+                            {/*        <div*/}
+                            {/*            className={'absolute px-4 w-full flex  items-center justify-between top-2 font-bold text-white z-10'}>*/}
+                            {/*            <KeyboardBackspaceIcon fontSize={'large'}*/}
+                            {/*                                   className={'cursor-pointer hover:bg-white hover:text-gray-800 rounded-2xl text-gray-200'}*/}
+                            {/*                                   onClick={props.toggleIsEditing}/>*/}
+                            {/*            <button*/}
+                            {/*                className={'bg-white text-gray-900 text-xl px-4 py-[2px] rounded-2xl z-10 hover:text-white hover:bg-gray-800 flex items-center'}*/}
+                            {/*                onClick={controlPosting}>*/}
+                            {/*                <span>Save</span>*/}
+                            {/*            </button>*/}
+                            {/*        </div>*/}
+                            {/*    </div>*/}
+                                {/*:*/}
+                                <div className={"bg-cover bg-no-repeat bg-center rounded-t-xl h-56 relative"}
+                                     id={"coverImageHolder"}
+                                     style={{backgroundImage:finishedCover?`url(${coverToCrop})`: `url(http://localhost:7070/${props.userProfile.coverImage})`}}>
+                                    <div
+                                        className={'absolute px-4 w-full flex  items-center justify-between top-2 font-bold text-white z-10'}>
+                                        <KeyboardBackspaceIcon fontSize={'large'}
+                                                               className={'cursor-pointer hover:bg-white hover:text-gray-800 rounded-2xl text-gray-200'}
+                                                               onClick={props.toggleIsEditing}/>
+                                        <button
+                                            className={'bg-white text-gray-900 text-xl px-4 py-[2px] rounded-2xl hover:text-white hover:bg-gray-800 flex items-center'}
+                                            onClick={controlPosting}>
+                                            <span>Save</span>
+                                        </button>
+                                    </div>
+
+                                    <div
+                                        className={'cursor-pointer relative w-full h-full flex rounded-t-xl flex-col text-white relative font-medium items-center justify-center bg-gray-700 opacity-80'}>
+                                        <CameraAltIcon fontSize={'large'} style={{fill: "white"}} className={'z-10'}/>
+                                        <p>Choose Cover</p>
+                                        <form id={'coverImageForm'} className={'w-full h-full absolute'}
+                                              encType={'multipart/form-data'}>
+                                            <input type={"file"}
+                                                   id={'coverImage'}
+                                                   className={'w-full opacity-0 left-0 absolute w-full z-10 h-full cursor-pointer'}
+                                                   onChange={(e) => setCoverImage(e.target.files[0])}/>
+                                        </form>
+                                    </div>
                                 </div>
+                            {/*}*/}
+                            {/*{finishedProfile ?*/}
+
+                            {/*    <div*/}
+                            {/*        className={'h-24 w-24 rounded-[50%] absolute top-[36%] left-[46.5%] shadow-xl bg-center bg-no-repeat bg-cover'}>*/}
+                            {/*        <img src={`${profileToCrop}`} alt="profile"*/}
+                            {/*             className="w-full h-full rounded-[50%]"/>*/}
+                            {/*    </div>*/}
+
+                            {/*    :*/}
+
                                 <div
-                                    className={'cursor-pointer relative w-full h-full flex rounded-t-xl flex-col text-white relative font-medium items-center justify-center bg-gray-700 opacity-80'}>
-                                    <CameraAltIcon fontSize={'large'} style={{fill: "white"}} className={'z-10'}/>
-                                    <p>Choose Cover</p>
-                                    <form id={'coverImageForm'} className={'w-full h-full absolute'}
-                                          encType={'multipart/form-data'}>
-                                        <input type={"file"}
-                                               id={'coverImage'}
-                                               className={'w-full opacity-0 left-0 absolute w-full z-10 h-full cursor-pointer'}
-                                               onChange={(e) => setCoverImage(e.target.files[0])}/>
-                                    </form>
+                                    className={'h-24 w-24 rounded-[50%] absolute top-[36%] left-[46.5%] shadow-xl bg-center bg-no-repeat bg-cover'}
+                                    style={{backgroundImage: finishedProfile?`url(${profileToCrop})`:`url(http://localhost:7070/${props.userProfile.profileImage})`,backgroundPosition:"center"}}>
+                                    <div
+                                        className={'cursor-pointer w-full h-full flex flex-col text-white rounded-[50%] relative font-medium items-center justify-center bg-gray-700 opacity-80'}>
+                                        <CameraAltIcon fontSize={'large'} style={{fill: "white"}}
+                                                       className={'opacity-100'}/>
+                                        <p>Profile</p>
+                                        <form id={'profileImageForm'}
+                                              className={' absolute w-full h-full rounded-[50%]'}>
+                                            <input type={"file"}
+                                                   id={'profileImage'}
+                                                   className={'w-full opacity-0 left-0 rounded-[50%] z-10 absolute w-full h-full cursor-pointer'}
+                                                   onChange={(e) => setProfileImage(e.target.files[0])}/>
+                                        </form>
+                                    </div>
                                 </div>
-                            </div>
-                            <div
-                                id={'profileImageHolder'}
-                                className={'h-24 w-24 rounded-[50%] absolute top-[36%] left-[46.5%] shadow-xl bg-center bg-no-repeat bg-cover'}
-                                style={{backgroundImage: `url(http://localhost:7070/${props.userProfile.profileImage})`}}>
-                                <div
-                                    className={'cursor-pointer w-full h-full flex flex-col text-white rounded-[50%] relative font-medium items-center justify-center bg-gray-700 opacity-80'}>
-                                    <CameraAltIcon fontSize={'large'} style={{fill: "white"}}
-                                                   className={'opacity-100'}/>
-                                    <p>Profile</p>
-                                    <form id={'profileImageForm'} className={' absolute w-full h-full rounded-[50%]'}>
-                                        <input type={"file"}
-                                               id={'profileImage'}
-                                               className={'w-full opacity-0 left-0 rounded-[50%] z-10 absolute w-full h-full cursor-pointer'}
-                                               onChange={(e) => setProfileImage(e.target.files[0])}/>
-                                    </form>
-                                </div>
-                            </div>
+                            {/*}*/}
                             <form
                                 className={'text-white relative top-20 px-5 pb-8 flex flex-col items-center justify-evenly h-64'}>
                                 <input
@@ -299,7 +329,6 @@ const EditProfile = (props) => {
                             </form>
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>

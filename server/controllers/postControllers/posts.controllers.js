d16f1c12595870d96331fe9e60e53d51f3602e08
Mutesa-Cedric <96414111+Mutesa-Cedric@users.noreply.github.com>
2022-05-06 12:11:57 +0200

    liking,commenting, saving and retweeping a post

--- server/controllers/postControllers/posts.controllers.js
+++ server/controllers/postControllers/posts.controllers.js
@@ -1,63 +1,66 @@
-const {postsSchema}=require('../../model/posts.schema')
+const {postsSchema} = require('../../model/posts.schema')
+const {indexOf} = require("lodash/array");
 //new post
-const newPost=async(req,res)=>{
+const newPost = async (req, res) => {
+    console.log(`posted at ${req.body.postedAt}`)
     try {
-        const {postedBy,text}=req.body;
-        const postedAt=req.time
-        const post=new postsSchema({
-            postedBy:postedBy,
-            text:text,
-            postedAt:postedAt,
-            media:req.file.filename
+        const {postedBy, text} = req.body;
+        const postedAt = req.body.postedAt
+
+        const post = new postsSchema({
+            postedBy: postedBy,
+            text: text,
+            postedAt: postedAt,
+            media: req.file.filename
             // media:req.file
         })
-        await post.save((err,post)=>{
-            if(err){
-               return res.json({
-                    status:500,
-                    success:false,
-                    message:"post not saved!"
+        await post.save((err, post) => {
+            if (err) {
+                return res.json({
+                    status: 500,
+                    success: false,
+                    message: "post not saved!"
                 })
-            }else{
+            } else {
                 res.json({
-                    status:201,
-                    saved:true,
-                    post:post,
-                    message:"post saved successfully"
+                    status: 201,
+                    saved: true,
+                    post: post,
+                    message: "post saved successfully"
                 })
             }
         })
     } catch (error) {
         return console.error(error)
-    } 
+    }
 }
 //new post
 
 //getting all posts
 
-const getAllPosts = async(req,res)=>{
+const getAllPosts = async (req, res) => {
     try {
-        await postsSchema.find((err,posts)=>{
-            if(err){
+        await postsSchema.find((err, posts) => {
+            if (err) {
                 throw err
-            }else{
-                if(posts.length===0){
+            } else {
+                if (posts.length === 0) {
                     res.json({
-                        status:404,
-                        areFound:false,
-                        message:"no posts found!"
+                        status: 404,
+                        areFound: false,
+                        message: "no posts found!"
                     })
-                }else{
+                } else {
                     res.json({
-                        status:200,
-                        success:true,
-                        numberOfPosts:posts.length,
-                        posts:posts
+                        status: 200,
+                        success: true,
+                        numberOfPosts: posts.length,
+                        posts: posts
                     })
                 }
-               
+
             }
-        })
+        }).sort({postedAt: -1})
     } catch (error) {
         return console.error(error)
     }
@@ -67,42 +70,191 @@ const getAllPosts = async(req,res)=>{
 
 //getting posts by username
 
-const getPostsByUsername = async(req,res)=>{
+const getPostsByUsername = async (req, res) => {
     try {
-        let userName=req.params.userName;
-        await postsSchema.find({postedBy:userName},(err,posts)=>{
+        let userName = req.params.userName;
+        await postsSchema.find({postedBy: userName}, (err, posts) => {
             if (err) {
                 return console.error(err)
             } else {
-                if(posts.length===0){
+                if (posts.length === 0) {
                     res.json({
-                        status:404,
-                        areFound:false,
-                        message:"no posts found!"
+                        status: 404,
+                        areFound: false,
+                        message: "no posts found!"
                     })
-                }else{
-                res.json({
-                    status:200,
-                    success:true,
-                    numberOfPosts:posts.length,
-                    posts:posts
-                })
+                } else {
+                    res.json({
+                        status: 200,
+                        success: true,
+                        numberOfPosts: posts.length,
+                        posts: posts
+                    })
+                }
             }
-        }
-        })
+        }).sort({postedAt: -1})
     } catch (error) {
-        return  console.error(error)   
+        return console.error(error)
     }
-     let userName=req.params.userName
+    let userName = req.params.userName
 }
 
 //getting posts by username
 
 //getting a post by _id
-let getPostById = async(req,res)=>{
+
+let getPostById = async (req, res) => {
     try {
-        let id=req.params.id;
-        await postsSchema.findById(id,(err,post)=>{
+        let id = req.params.id;
+        await postsSchema.findById(id, (err, post) => {
+            if (err) {
+                res.json({
+                    status: 404,
+                    success: false,
+                    message: "post not found!"
+                })
+            } else {
+                res.json({
+                    status: 200,
+                    success: true,
+                    post: post
+                })
+            }
+        })
+    } catch (error) {
+        return console.error(error)
+    }
+}
+
+//getting a post by _id
+
+//updating a post
+
+const updatePost = async (req, res) => {
+    try {
+        let postId = req.params.id;
+        let updates = req.body;
+
+        if (!await postsSchema.updateOne({_id: postId}, updates, (err, post) => {if (err) {
+                res.json({
+                    status: 404,
+                    success: false,
+                    message: "post not found!"
+                })
+            } else {
+                res.json({
+                    status: 204,
+                    updated: true,
+                    updatedPost: post
+                })
+            }
+        })) {
+            res.json({
+                status: 404,
+                success: false,
+                message: "post not found!"
+            })
+        }
+
+    } catch (error) {
+        return console.error(error)
+    }
+}
+
+
+//updating a post
+
+// updating post comments
+
+const updateComments = async (req, res) => {
+    try {
+        let postId = req.params.id
+        let {comments} = req.body
+        if (await postsSchema.updateOne({_id: postId}, {$push: {comments: comments}})) {
+            res.json({
+                status: 203,
+                message: "updated!",
+                success: true
+            })
+        }
+    } catch (error) {
+        res.json({
+            message: error
+        })
+    }
+}
+
+// updating post comments
+
+
+//updating retweeps
+
+const updateRetweeps = async (req, res) => {
+    try {
+       let postId=req.params.id;
+       let {retweep}=req.body;
+
+       await postsSchema.findOne({_id: postId},(err,post)=>{
+           if(err){
+               res.json({
+                   status:404,
+                   success:false,
+                   message:"post not found!"
+               })
+           }else{
+               if(post.retweeps.includes(retweep)){
+                   let retweepIndex=post.retweeps.indexOf(retweep);
+                   post.retweeps.splice(retweepIndex,1);
+
+                   post.save((err,post)=>{
+                       if(err){
+                           res.json({
+                               status:404,
+                               success:false,
+                               message:"post not found!"
+                           })
+                       }else{
+                           res.json({
+                               status:200,
+                               success:true,
+                               message:"retweet removed!",
+                               post:post
+                           })
+                       }
+                   })
+               }else{
+                   postsSchema.updateOne({_id:postId},{$push:{retweeps:retweep}},(err,post)=> {
+                       if (err){
+                        return res.json({
+                               status:404,
+                               success:false,
+                               message:"post not found!"
+                           })
+                       }
+                       res.json({
+                           status: 203,
+                           message: "updated!",
+                           success: true
+                       })
+                   })
+               }
+           }
+       })
+    }catch(error){
+    }
+}
+
+//updating retweeps
+
+//updating likes
+
+const updateLikes=async (req, res) => {
+    try {
+
+        let postId=req.params.id;
+        let {like}=req.body;
+
+        await postsSchema.findOne({_id: postId},(err,post)=>{
             if(err){
                 res.json({
                     status:404,
@@ -110,103 +262,141 @@ let getPostById = async(req,res)=>{
                     message:"post not found!"
                 })
             }else{
-                res.json({
-                    status:200,
-                    success:true,
-                    post:post
-                })
+                if(post.likes.includes(like)){
+                    let likeIndex=post.likes.indexOf(like);
+                    post.likes.splice(likeIndex,1);
+
+                    post.save((err,post)=>{
+                        if(err){
+                            res.json({
+                                status:404,
+                                success:false,
+                                message:"post not found!"
+                            })
+                        }else{
+                            res.json({
+                                status:200,
+                                success:true,
+                                message:"like removed successfully!",
+                                remainingLikes:post.likes.length
+                            })
+                        }
+                    })
+                }else{
+                    postsSchema.updateOne({_id:postId},{$push:{likes:like}},(err,post)=> {
+                        if (err){
+                            return res.json({
+                                status:404,
+                                success:false,
+                                message:"post not found!"
+                            })
+                        }
+                        res.json({
+                            status: 203,
+                            message: "like added successfully!",
+                            success: true
+                        })
+                    })
+                }
             }
         })
-    } catch (error) {
-        return console.error(error)
+     }catch(error){
+     }
+}
+//updating likes
+
+//update saved
+const updateSaved=async(req,res)=>{
+    try {
+
+        let postId=req.params.id;
+        let {save}=req.body;
+
+        await postsSchema.findOne({_id: postId},(err,post)=>{
+          if(err){
+              res.json({
+                  status:404,
+                  success:false,
+                  message:"post not found!"
+              })
+          }else{
+              if(post.saved.includes(save)){
+                  let saveIndex=post.saved.indexOf(save);
+                  post.saved.splice(saveIndex,1);
+                  post.save((err,post)=>{
+                      if(err){
+                          res.json({
+                              status:404,
+                              success:false,
+                              message:"post not found!"
+                          })
+                      }else{
+                          res.json({
+                              status:200,
+                              success:true,
+                              message:"save removed successfully!",
+                              remainingSaves:post.saved.length
+                          })
+                      }
+                  })
+              }else{
+                  postsSchema.updateOne({_id:postId},{$push:{saved:save}},(err,post)=> {
+                      if (err){
+                          return res.json({
+                              status:404,
+                              success:false,
+                              message:"post not found!"
+                          })
+                      }
+                      res.json({
+                          status: 203,
+                          message: "save added successfully!",
+                          success: true
+                      })
+                  })
+              }
+          }
+
+        })
+
+    }catch(error){
     }
 }
+//update saved
 
-//getting a post by _id
+//deleting a post
 
-//updating a post
-
-const updatePost = async(req,res)=>{
+const deletePost = async (req, res) => {
     try {
-        let postId=req.params.id;
-        let updates=req.body;
-        await postsSchema.findByIdAndUpdate(postId,updates,{new:true},(err,post)=>{
+        let id = req.params.id;
+        await postsSchema.findByIdAndDelete(id, (err, data) => {
             if (err) {
                 res.json({
-                    status: 404,
-                    success:false,
-                    message:"post not found!"
+                    status: 400,
+                    success: false,
+                    message: "post not found!"
                 })
             } else {
                 res.json({
-                    status:204,
-                    updated:true,
-                    updatedPost:post
+                    status: 200,
+                    deleted: true,
+                    deletedPost: data
                 })
             }
         })
-        
-    } catch (error) {
-        return console.error(error)
-    }
-}
-
-
-//updating a post
-
-// updateting post comments
-
-const updateComments=async(req,res) => {
-    try {
-        let postId = req.params.id
-        let {comments}=req.body
-            if(await postsSchema.updateOne({_id: postId}, {$push: {comments:comments}})){
-                res.json({
-                    status:203,
-                    message:"updated!"
-                })
-                }    
-        } catch (error) {
-            res.json({
-                message:error
-            })
-        }
-}
-
-// updateting post comments
-
-
-
-//deleting a post
-
-const deletePost= async(req,res)=>{
-    try {
-        let id=req.params.id;
-        await postsSchema.findByIdAndDelete(id,(err,data)=>{
-        if(err){
-            res.json({
-                status:400,
-                success:false,
-                message:"post not found!"
-            })
-        }else{
-            res.json({
-                status:200,
-                deleted:true,
-                deletedPost:data
-            })
-        }
-        })
     } catch (error) {
         return console.error(error)
     }
 }
 
 //deleting a post
-module.exports.getPostById=getPostById;
-module.exports.newPost=newPost;
-module.exports.getAllPosts=getAllPosts;
-module.exports.getPostsByUsername=getPostsByUsername;
-module.exports.updatePost=updatePost;
-module.exports.deletePost=deletePost;
-module.exports.updateComments=updateComments;
+module.exports.getPostById = getPostById;
+module.exports.newPost = newPost;
+module.exports.getAllPosts = getAllPosts;
+module.exports.getPostsByUsername = getPostsByUsername;
+module.exports.updatePost = updatePost;
+module.exports.deletePost = deletePost;
+module.exports.updateComments = updateComments;
+module.exports.updateRetweeps = updateRetweeps;
+module.exports.updateLikes = updateLikes;
+module.exports.updateSaved=updateSaved;

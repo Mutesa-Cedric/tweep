6d914f17edce8a204be0c0d9cdbc217578b36628
Mutesa-Cedric <96414111+Mutesa-Cedric@users.noreply.github.com>
2022-05-08 21:35:31 +0200

    modified bookmarks page

--- client/src/pages/bookmarks/Bookmarks.js
+++ client/src/pages/bookmarks/Bookmarks.js
@@ -1,64 +1,113 @@
-
 import Navbar from '../../components/Navbar';
-import React , {useState,useEffect} from "react";
+import React, {useState, useEffect} from "react";
 import {useNavigate} from 'react-router-dom';
 import CircularProgress from '@mui/material/CircularProgress';
 import SideSection from '../../components/SideSection';
 import Post from '../../components/Post';
-import post from "../../images/post.jpg";
-import post2 from "../../images/post2.jpg";
-import tweeper from "../../images/tweeper.jpg";
-import Lois from "../../images/Lois.jpg";
+import {Link} from 'react-router-dom';
 
-let Bookmarks=(props)=>{
+let Bookmarks = (props) => {
 
-    let navigate=useNavigate()
+    let navigate = useNavigate()
     const [userProfile, setUserProfile] = useState({})
     const [hasProfile, setHasProfile] = useState(false)
+    const [savedPosts, setSavedPosts] = useState([]);
+    const [hasNoSavedPosts, setHasNoSavedPosts] = useState(false);
+
+    console.log(savedPosts)
     //checking if the user is logged in 
-    useEffect(()=>{
-        const accessToken=window.localStorage.getItem("accessToken")
-        if(!accessToken){
+    useEffect(() => {
+        const accessToken = window.localStorage.getItem("accessToken")
+        if (!accessToken) {
             navigate('/auth/signup')
-        }else{
+        } else {
             fetch(`http://localhost:7070/auth/verifyToken/${accessToken}`)
-            .then(response=>response.json())
-            .then(data=>{
-                if(!data.authorized){
-                    navigate('/auth/login')
-                }else{
-                   fetch(`http://localhost:7070/profiles/${data.user.userName}`)
-                   .then(response=>response.json())
-                   .then(data=>{
-                      setUserProfile(data.profile)
-                      setHasProfile(true)
-                   }) 
+                .then(response => response.json())
+                .then(data => {
+                    if (!data.authorized) {
+                        navigate('/auth/login')
+                    } else {
+                        fetch(`http://localhost:7070/profiles/${data.user.userName}`)
+                            .then(response => response.json())
+                            .then(data => {
+                                setUserProfile(data.profile)
+                                setHasProfile(true)
+                            })
+                    }
+                })
+                .catch(err => console.error(err))
+        }
+    }, [])
+
+    useEffect(() => {
+        if (hasProfile) {
+            fetch(`http://localhost:7070/posts/getSavedTweeps/${userProfile.userName}`).then(response => response.json()).then(data => {
+                if (data.posts.length === 0) {
+                    setHasNoSavedPosts(true)
+                } else {
+                    setSavedPosts(data.posts)
                 }
             })
-            .catch(err=>console.error(err))
         }
-    },[])
+    }, [hasProfile])
 
-    if(hasProfile){
+
+    const postElements = savedPosts.map(post => {
+        return <Post
+            key={post._id}
+            likesArray={post.likes}
+            likes={post.likes.length}
+            postId={post._id}
+            darkMode={props.darkMode}
+            name={post.postedBy}
+            profile={`http://localhost:7070/${post.postedBy}Profile.png`}
+            createdAt={new Date(post.postedAt).toDateString()} text={post.text}
+            img={`http://localhost:7070/${post.media}`}
+            comments={post.comments.length}
+            retweeps={post.retweeps.length}
+            retweepsArray={post.retweeps}
+            saves={post.saved.length}
+            savesArray={post.saved}
+            commentsArray={post.comments}
+            image={`http://localhost:7070/${`${userProfile.profileImage}`}`}
+            currentUser={userProfile.userName}
+        />
+    })
+
+    if (hasProfile) {
         return (
-            <div className={props.darkMode?"bg-[#252329] h-screen overflow-x-hidden":"bg-[#F2F2F2] h-screen overflow-x-hidden"}>
-                {userProfile.profileImage ? <Navbar toBookmarks={true} darkMode={props.darkMode} setDarkMode={props.setDarkMode} profileImg={`http://localhost:7070/${`${userProfile.profileImage}`}`} userName={userProfile.userName}/>:<Navbar darkMode={props.darkMode} setDarkMode={props.setDarkMode} userName={userProfile.userName}/>}
-                <div className=" mt-20 flex justify-between  xl:px-52 ">
-                    <div>
-                      <SideSection darkMode={props.darkMode} fixLinks={props.fixSideSearch}/>
+            <div
+                className={props.darkMode ? "bg-[#252329] h-screen overflow-x-hidden" : "bg-[#F2F2F2] h-screen overflow-x-hidden"}>
+                {userProfile.profileImage ?
+                    <Navbar toBookmarks={true} darkMode={props.darkMode} setDarkMode={props.setDarkMode}
+                            profileImg={`http://localhost:7070/${`${userProfile.profileImage}`}`}
+                            userName={userProfile.userName}/> :
+                    <Navbar darkMode={props.darkMode} setDarkMode={props.setDarkMode} userName={userProfile.userName}/>}
+                <div className=" mt-32 flex justify-between xl:px-52 ">
+                    <div className={'mr-24'}>
+                        <SideSection darkMode={props.darkMode} fixLinks={props.fixSideSearch}/>
                     </div>
-                    <div className="w-[745px]" >
-                        <Post darkMode={props.darkMode} name="Peyton Lyons" profile={tweeper} createdAt='24 August at 20:43' text="Traveling-it leaves you speechless, then turns you into a storyteller." img={post} comments={449} retweeps={59004} saves={234}/>
-                        <Post darkMode={props.darkMode} name="Bianca Lois" profile={Lois} createdAt='24 August at 20:43' text="“We travel, some of us forever, to seek other places, other lives, other souls.” – Anais Nin" img={post2} comments={208} retweeps={2764} saves={178}/>
-                        <Post darkMode={props.darkMode} name="Mikael Stanley" createdAt='13 September at 12:41' text="Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking. -Steve Jobs" comments={23} retweeps={10} saves={20}/>
+                    <div className="w-[745px]">
+
+                        {hasNoSavedPosts ?
+                             <div className={props.darkMode?"w-full h-full border-[1px] bg-inherit shadow-md flex items-center flex-col justify-center":'w-full h-full border-2 bg-gray-100 flex items-center flex-col justify-center'}>
+                                <span className={'text-gray-500 text-lg capitalize'}>You have no saved Tweeps.</span>
+                                 <Link to={'/'}>
+                                     <button className={'bg-blue-400 hover:bg-blue-500 my-4 text-white px-4 py-1 rounded-sm'}>
+                                         <span className={'capitalize '}>go save one</span>
+                                     </button>
+                                 </Link>
+
+                            </div> : postElements}
                     </div>
                 </div>
             </div>
         )
-    }else{
+    } else {
         return (
-            <div className={props.darkMode?"w-full bg-[#252329] h-screen flex items-center justify-center":"w-full h-screen flex items-center justify-center"}>
-                 <CircularProgress/>
+            <div
+                className={props.darkMode ? "w-full bg-[#252329] h-screen flex items-center justify-center" : "w-full h-screen flex items-center justify-center"}>
+                <CircularProgress/>
             </div>
         )
     }
